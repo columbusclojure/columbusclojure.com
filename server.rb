f296a1b88d58c520598aebe4c6bf4ce31e203677
Matthew Boston <matthew.boston@gmail.com>
2012-03-25 18:43:59 -0400

    ws

--- server.rb
+++ server.rb
@@ -24,7 +24,7 @@ get '/' do
   date          = @next_meeting.strftime('%D')
   @speakers     = info[date]['speakers'] || []
   @mailing_list = "http://groups.google.com/group/columbusclojure"
-  
+
   haml :welcome
 end
 
