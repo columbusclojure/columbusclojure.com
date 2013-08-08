4beb1f9e1ef256c2cbf6aa6077c63b0ed5dcfdba
Matthew Boston <matthew.boston@gmail.com>
2013-08-07 23:49:34 -0400

    fix next meeting calculation if next meeting is today

--- lib/meeting_dates.rb
+++ lib/meeting_dates.rb
@@ -4,10 +4,16 @@ require 'chronic'
 
 class MeetingDates
   SIX_OCLOCK = 18.hours
+  WEDNESDAY = 3
 
   def self.next
-    if Time.now.day <= 7
-      Meeting.new(Chronic.parse('first wednesday this month').beginning_of_day + SIX_OCLOCK)
+    now = Time.now
+    if now.day <= 7
+      if now.wday == WEDNESDAY
+        Meeting.new(now.beginning_of_day + SIX_OCLOCK)
+      else
+        Meeting.new(Chronic.parse('next wednesday').beginning_of_day + SIX_OCLOCK)
+      end
     else
       Meeting.new(Chronic.parse('first wednesday next month').beginning_of_day + SIX_OCLOCK)
     end
--- test/test_meeting_dates.rb
+++ test/test_meeting_dates.rb
@@ -6,17 +6,12 @@ require 'meeting_dates'
 require 'timecop'
 
 class TestMeetingDates < Minitest::Test
-  WEDNESDAY = 3
   def setup
     @next_meeting_date = MeetingDates.next.date
   end
 
   def test_next_meeting_is_wednesday
-    assert_equal WEDNESDAY, @next_meeting_date.wday
-  end
-
-  def test_next_meeting_is_within_the_first_week
-    assert_equal (@next_meeting_date - 7.days).month + 1, @next_meeting_date.month
+    assert_equal MeetingDates::WEDNESDAY, @next_meeting_date.wday
   end
 
   def test_next_meeting_is_six_oclock
@@ -24,10 +19,25 @@ class TestMeetingDates < Minitest::Test
     assert_equal 0, @next_meeting_date.min
   end
 
-  def test_next_meeting_can_be_this_month
+  def test_next_meeting_is_today
+    Timecop.freeze(Time.new(2013, 8, 7)) do
+      assert_equal 8, MeetingDates.next.date.month
+      assert_equal 7, MeetingDates.next.date.day
+    end
+    Timecop.return
+  end
+
+  def test_next_meeting_is_within_the_first_week
     Timecop.freeze(Time.new(2013, 8, 1)) do
-      next_meeting_date = MeetingDates.next.date
-      assert_equal 8, next_meeting_date.month
+      assert_equal 8, MeetingDates.next.date.month
+      assert_equal 7, MeetingDates.next.date.day
+    end
+    Timecop.return
+  end
+
+  def test_next_meeting_is_next_month
+    Timecop.freeze(Time.new(2013, 8, 10)) do
+      assert_equal 9, MeetingDates.next.date.month
     end
     Timecop.return
   end
