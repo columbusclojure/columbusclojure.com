29257dbe18038279514766996581c4f1bcdb75c5
xandrews <john@neo.com>
2013-10-08 12:49:32 -0400

    Add October meeting time.
    Use next defined meeting time if available.
    Sorry so ugly :-P

--- lib/meeting_dates.rb
+++ lib/meeting_dates.rb
@@ -8,14 +8,20 @@ class MeetingDates
 
   def self.next
     now = Time.now
-    if now.day <= 7
-      if now.wday == WEDNESDAY
-        Meeting.new(now.beginning_of_day + SIX_OCLOCK)
-      else
-        Meeting.new(Chronic.parse('next wednesday').beginning_of_day + SIX_OCLOCK)
-      end
+    future_meetings = Meeting.dates.select {|d| d > Date.today}
+    next_meeting = future_meetings.sort.first
+    if next_meeting
+      Meeting.new(next_meeting.to_time + SIX_OCLOCK)
     else
-      Meeting.new(Chronic.parse('first wednesday next month').beginning_of_day + SIX_OCLOCK)
+      if now.day <= 7
+        if now.wday == WEDNESDAY
+          Meeting.new(now.beginning_of_day + SIX_OCLOCK)
+        else
+          Meeting.new(Chronic.parse('next wednesday').beginning_of_day + SIX_OCLOCK)
+        end
+      else
+        Meeting.new(Chronic.parse('first wednesday next month').beginning_of_day + SIX_OCLOCK)
+      end
     end
   end
 end
