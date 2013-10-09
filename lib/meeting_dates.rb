bccbdda476e8b37ecdbe8055b0ea5c6033814d90
Matthew Boston <matthew.boston@gmail.com>
2013-10-09 16:52:34 -0400

    update meetings

--- lib/meeting_dates.rb
+++ lib/meeting_dates.rb
@@ -8,20 +8,14 @@ class MeetingDates
 
   def self.next
     now = Time.now
-    future_meetings = Meeting.dates.select {|d| d > Date.today}
-    next_meeting = future_meetings.sort.first
-    if next_meeting
-      Meeting.new(next_meeting.to_time + SIX_OCLOCK)
-    else
-      if now.day <= 7
-        if now.wday == WEDNESDAY
-          Meeting.new(now.beginning_of_day + SIX_OCLOCK)
-        else
-          Meeting.new(Chronic.parse('next wednesday').beginning_of_day + SIX_OCLOCK)
-        end
+    if now.day <= 7
+      if now.wday == WEDNESDAY
+        Meeting.new(now.beginning_of_day + SIX_OCLOCK)
       else
-        Meeting.new(Chronic.parse('first wednesday next month').beginning_of_day + SIX_OCLOCK)
+        Meeting.new(Chronic.parse('next wednesday').beginning_of_day + SIX_OCLOCK)
       end
+    else
+      Meeting.new(Chronic.parse('first wednesday next month').beginning_of_day + SIX_OCLOCK)
     end
   end
 end
