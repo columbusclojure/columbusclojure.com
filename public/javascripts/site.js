670435cd307c4e6d1360b3f06e5d752ace664d00
Matthew Boston <matthew.boston@gmail.com>
2013-12-31 12:37:11 -0500

    namespace App.Tweets

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,65 +1,10 @@
 $(function() {
-  _.each($('.thirds'), function(outer) {
-    $(outer).children().css('height', $(outer).height());
-  });
-
   $('.when-link').on('click', highlightWhen);
   $('.join-link').on('click', highlightJoin);
 
-  loadTweets();
+  resizeThirds();
+  App.Tweets.loadTweets();
 });
-
-var green = '#91dc47';
-
-function highlightId (id, e) {
-  $('#'+id).effect('highlight', {'color': green}, 3000);
-}
-
-function highlightWhen (e) {
-  e.preventDefault();
-  highlightId('when');
-}
-
-function highlightJoin (e) {
-  e.preventDefault();
-  highlightId('join');
-}
-
-function linkUrls(text) {
-  var urlRegex = /(https?\:\/\/[^"\s\>]*[^.,;'">\:\s\>\)\]\!])/g;
-  return text.replace(urlRegex, "<a href='$1'>$1</a>");
-}
-
-function linkMentions(text) {
-  var urlRegex = /@(\w+)/g;
-  return text.replace(urlRegex, "<a href='http://twitter.com/$1'>@$1</a>");
-}
-
-function linkHashtags(text) {
-  var urlRegex = /#(\w+)/g;
-  return text.replace(urlRegex, "<a href='https://twitter.com/search/%23$1'>#$1</a>");
-}
-
-function addLinksToTweets(text){
-  text = linkUrls(text);
-  text = linkMentions(text);
-  return linkHashtags(text);
-}
-
-function drawTweet(tweet) {
-  var $tweets    = $('#tweets');
-  var template   = $('#tweetTemplate').html();
-
-  if (!_.isUndefined(tweet)) {
-    var text  = addLinksToTweets(tweet.text);
-    var time  = Date.create(tweet.created_at).relative();
-    var html  = _.template(template, {tweet: text, time: time});
-    var div   = $('<div>').html(html);
-    div.addClass('tweet');
-    div.addClass('half');
-    $tweets.append(div);
-  }
-}
 
 function resizeThirds() {
   _.each($('.thirds'), function(outer) {
@@ -67,8 +12,66 @@ function resizeThirds() {
   });
 }
 
-function loadTweets() {
-  $.get('/tweets', function(tweets) {
-    _.each(tweets, drawTweet);
-  });
+var App = {};
+
+App.Colors = {
+  green: '#91dc47'
+};
+
+function highlight (selector) {
+  $(selector).effect('highlight', {'color': App.Colors.green}, 3000);
 }
+
+function highlightWhen (e) {
+  e.preventDefault();
+  highlight('#when');
+}
+
+function highlightJoin (e) {
+  e.preventDefault();
+  highlight('#join');
+}
+
+App.Tweets = {
+  linkUrls: function(text) {
+    var urlRegex = /(https?\:\/\/[^"\s\>]*[^.,;'">\:\s\>\)\]\!])/g;
+    return text.replace(urlRegex, "<a href='$1'>$1</a>");
+  },
+
+  linkMentions: function(text) {
+    var urlRegex = /@(\w+)/g;
+    return text.replace(urlRegex, "<a href='http://twitter.com/$1'>@$1</a>");
+  },
+
+  linkHashtags: function(text) {
+    var urlRegex = /#(\w+)/g;
+    return text.replace(urlRegex, "<a href='https://twitter.com/search/%23$1'>#$1</a>");
+  },
+
+  addLinksToTweets: function(text) {
+    text = this.linkUrls(text);
+    text = this.linkMentions(text);
+    return this.linkHashtags(text);
+  },
+
+  drawTweet: function(tweet) {
+    if (!_.isUndefined(tweet)) {
+      var $tweets  = $('#tweets');
+      var template = $('#tweetTemplate').html();
+      var text     = this.addLinksToTweets(tweet.text);
+      var time     = Date.create(tweet.created_at).relative();
+      var html     = _.template(template, {tweet: text, time: time});
+      var div      = $('<div>').html(html).addClass('tweet half');
+      $tweets.append(div);
+    }
+  },
+
+  loadTweets: function() {
+    var self = this;
+    $.get('/tweets', function(tweets) {
+      _.each(tweets, function(tweet) {
+        self.drawTweet(tweet);
+      });
+    });
+  }
+};
